27c9503a69212a9aae27c5d4da9ceea6863385e4
melamela <p500297@gmail.com>
2023-10-09 00:23:56 +0800

    update

--- 2023_09_16/Mr_Kitayuta_Technology.cpp
+++ 2023_09_16/Mr_Kitayuta_Technology.cpp
@@ -3,33 +3,57 @@
 #include <algorithm>
 using namespace std;
 int n, m, a, b;
-vector<int> v[100005], rv[100005];
-bool vis[100005], vis2[100005];
+bool flag = 0;
+vector<int> v[100005], cv[100005], path;
+int vis[100005], vis2[100005];
 void dfs(int x){
     vis[x] = 1;
-    for(auto i : v[x]){
+    path.push_back(x);
+    for(auto i : cv[x]){
         if(!vis[i]) dfs(i);
     }
 }
 void dfs2(int x){
-    vis2[x] = 1;
-    for(auto i : rv[x]){
-        if(!vis[i]) dfs2(i);
+    if(vis2[x]){
+        if(vis2[x] == 1) flag = 1;
+        return;
     }
+    vis2[x] = 1;
+    for(auto i : v[x]){
+        dfs2(i);
+    }
+    vis2[x] = 2;
 }
 int main(){
+    // think:
+    // cycle with k nodes --> need k paths
+    // otherwise, need connected compoent k - 1 paths
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> m;
     while(m--){
         cin >> a >> b;
         v[a].push_back(b);
-        rv[b].push_back(a);
+        cv[a].push_back(b);
+        cv[b].push_back(a);
     }
-    dfs(1);
-    dfs2(1);
+    // undirect graph avoid dfs uncompletely
+    // ex: 6 -> 1 -> 3
+    // if use direct graph and start dfs from 1
+    // you will get path: 1 -> 3, 6
+    // each path does not include cycle --> ans = (2 - 1) + (1 - 1) = 1
+    // actually ans is "2"
     int ans = 0;
     for(int i = 1; i <= n; i++){
-        if(!vis[i] || !vis2[i]) ans += 1;
+        if(!vis[i]){
+            flag = 0;
+            dfs(i); // find conected component
+            ans += path.size();
+            for(auto e : path){
+                if(!vis2[e]) dfs2(e); // find cycle
+            }
+            if(!flag) ans -= 1;
+            path.clear();
+        }
     }
     cout << ans << '\n';
 }
